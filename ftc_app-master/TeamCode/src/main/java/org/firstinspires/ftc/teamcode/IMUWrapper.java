a2e5a2bfb774cc8b09bc03adf2013e73239b89fa
Rohan Viswanathan <rohan.viswanathan@warriorlife.net>
2017-09-20 18:26:50 -0700

    Starting imu wrapper class.

--- ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IMUWrapper.java
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IMUWrapper.java
@@ -1,7 +1,16 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.ReadWriteFile;
+
+import org.firstinspires.ftc.robotcore.external.navigation.Acceleration;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AngularVelocity;
+import org.firstinspires.ftc.robotcore.internal.system.AppUtil;
+
+import java.io.File;
 
 /**
  * Created by Team 3256 on 9/20/2017.
@@ -12,6 +21,9 @@ public class IMUWrapper {
     private String name;
     private HardwareMap hardwareMap;
     private BNO055IMU imu;
+    private double distanceFromZero;
+
+    BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
 
     public IMUWrapper(String name, HardwareMap hardwareMap){
         this.name = name;
@@ -19,16 +31,33 @@ public class IMUWrapper {
     }
 
     public void initialize() {
+
         imu = hardwareMap.get(BNO055IMU.class, name);
+        parameters.angleUnit           = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit           = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.calibrationDataFile = "BNO055IMUCalibration.json"; // see the calibration sample opmode
+        parameters.loggingEnabled      = true;
+        parameters.loggingTag          = "IMU";
+        parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
+        imu.initialize(parameters);
+
     }
 
     public void calibrate() {
 
+        BNO055IMU.CalibrationData calibrationData = imu.readCalibrationData();
+        String filename = "AdafruitIMUCalibration.json";
+        File file = AppUtil.getInstance().getSettingsFile(filename);
+        ReadWriteFile.writeFile(file, calibrationData.serialize());
+
     }
 
-    public void reset() {}
-
-    public double getHeading() {
-        return 0;
+    public AngularVelocity getHeading() {
+        return imu.getAngularVelocity();
     }
+
+    public void reset() {
+
+    }
+
 }
