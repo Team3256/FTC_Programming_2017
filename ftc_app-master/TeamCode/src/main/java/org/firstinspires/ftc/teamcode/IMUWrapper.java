2967c15c15afd13d82114c605b961b2527cf4fdb
Rohan Viswanathan <rohan.viswanathan@warriorlife.net>
2017-09-25 18:14:05 -0700

    Fixed IMUWrapper, calibrates ok. Still need to fix reset.

--- ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IMUWrapper.java
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IMUWrapper.java
@@ -8,9 +8,15 @@ import com.qualcomm.robotcore.util.ReadWriteFile;
 import org.firstinspires.ftc.robotcore.external.navigation.Acceleration;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.AngularVelocity;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
 import org.firstinspires.ftc.robotcore.internal.system.AppUtil;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 
 import java.io.File;
+import java.util.Locale;
+
+import static android.R.attr.angle;
 
 /**
  * Created by Team 3256 on 9/20/2017.
@@ -21,8 +27,9 @@ public class IMUWrapper {
     private String name;
     private HardwareMap hardwareMap;
     private BNO055IMU imu;
-    private double heading;
-    private double difference;
+    private double offset = 0;
+    private double angle = 0;
+
 
     BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
 
@@ -43,18 +50,20 @@ public class IMUWrapper {
         imu.initialize(parameters);
 
         BNO055IMU.CalibrationData calibrationData = imu.readCalibrationData();
-        String filename = "AdafruitIMUCalibration.json";
+        String filename = "BNO055IMUCalibration.json";
         File file = AppUtil.getInstance().getSettingsFile(filename);
         ReadWriteFile.writeFile(file, calibrationData.serialize());
 
     }
 
-    public AngularVelocity getHeading() {
+    public double getHeading() {
 
-        return imu.getAngularVelocity();
+        return angle = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES).firstAngle - offset;
     }
 
     public void reset() {
+        offset = angle;
+
     }
 
 }
--- ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test.java
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test.java
@@ -12,32 +12,49 @@ import com.qualcomm.robotcore.hardware.Gyroscope;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
+import static com.sun.tools.doclint.HtmlTag.I;
+
 /**
  * Created by Team 3256 on 9/20/2017.
  */
 @TeleOp
 public class Test extends LinearOpMode {
+
+
     @Override
     public void runOpMode() throws InterruptedException {
 
         //DistanceSensor sensorColorRange = hardwareMap.get(DistanceSensor.class, "sensorColorRange");
         //ColorSensor colorSensor = hardwareMap.get(ColorSensor.class, "sensorColorRange");
-        //Gyroscope imu = hardwareMap.get(Gyroscope.class, "imu");
+        IMUWrapper imu = new IMUWrapper("imu", hardwareMap);
+        imu.calibrate();
+
+
         super.waitForStart();
 
-        /*while(opModeIsActive()) {
-            telemetry.addData("Distance (cm)", sensorColorRange.getDistance(DistanceUnit.CM));
+        while (opModeIsActive()) {
+
+
+
+
+
+            /*telemetry.addData("Distance (cm)", sensorColorRange.getDistance(DistanceUnit.CM));
             telemetry.addData("Color: Red", colorSensor.red());
             telemetry.addData("Color: Green", colorSensor.green());
             telemetry.addData("Color: Blue", colorSensor.blue());
+            telemetry.update(); */
+            telemetry.addData("Gyro:", imu.getHeading());
             telemetry.update();
-            telemetry.addData("Gyro:", imu.getAngularVelocity(AngleUnit.DEGREES));
-            telemetry.update();*/
 
+            if (gamepad1.left_bumper){
 
+                imu.reset();
+
+            }
 
 
         }
 
     }
 
+}
